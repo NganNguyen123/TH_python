e4277010f36547b32e2391d36d9f2788b0d2aef3
Ngan Nguyen <ngannguyen.dhnt3@gmail.com>
2018-04-23 18:34:35 -0700

    add

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+	Abc("Ngan cho  ");
+	ngan();
+	printf("Ngan cho \n");
+	return 1;
+}
---
+++ program.py
@@ -0,0 +1,8 @@
+print("nhap ten: ")
+Ten=input()
+print("nhap nam sinh: ")
+Namsinh=int(input())
+print("nhap khoa: ")
+Khoa=int(input())
+print (Ten,Namsinh,Khoa)
+
