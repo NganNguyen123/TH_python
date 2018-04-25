3bdafb012b85490f7fe74629b6e2b7b82b020b9c
NganNguyen <38675440+NganNguyen123@users.noreply.github.com>
2018-04-25 14:44:20 +0700

    Update program.py

--- program.py
+++ program.py
@@ -1,8 +1,15 @@
-print("nhap ten: ")
-Ten=input()
-print("nhap nam sinh: ")
-Namsinh=int(input())
-print("nhap khoa: ")
-Khoa=int(input())
-print (Ten,Namsinh,Khoa)
+from sinhvien import SinhVien
+
+a = []
+n = int(input('Nhap so luong Sinh Vien can nhap : '))
+x = n
+while(x > 0):
+	ten = raw_input( 'Nhap ten SV : ')
+	namsinh = raw_input('Nhap nam sinh : ')
+	khoa = raw_input('Nhap khoa : ')
+	a.append(SinhVien(ten,namsinh,khoa))
+	x = x - 1 
+for abc in a:
+	abc.toString()
+
 
